fc2198d0f3b598b4c69f97b828d5ce6aa76fb4f5
Marc Gilleron <marc.gilleron@gmail.com>
2026-05-14 21:20:08 +0200

    Move FX name to base class

--- zylann.rpp/rpp_fx.gd
+++ zylann.rpp/rpp_fx.gd
@@ -1,3 +1,5 @@
 class_name RPP_Fx
 
+var name: String = ""
+var filename: String = ""
 var param_envelopes: Array[RPP_ParamEnvelope] = []
--- zylann.rpp/rpp_vst.gd
+++ zylann.rpp/rpp_vst.gd
@@ -1,4 +1,1 @@
 class_name RPP_Vst extends RPP_Fx
-
-var name: String = ""
-var filename: String = ""
